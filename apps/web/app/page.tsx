30afc4cb1e015cef20b8c99524646937290bbed7
ParwinderBhullar <harshankhangura@gmail.com>
2026-08-22 22:59:49 -0400

    Update user display format on home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,5 +3,5 @@ import { prisma } from "@repo/db";
 
 export default async function Home() {
   const user = await prisma.user.findFirst();
-  return <div className={styles.page}>{user?.username ?? "No user added yet"}</div>;
+  return <div className={styles.page}>Name:Parwinder {user?.username ?? "No user added yet"}</div>;
 }
